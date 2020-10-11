d6b46b5c29748086f9543a17d74bf16056f61ca7
kansei-bit <kansei1001@gmail.com>
2020-10-11 15:50:36 +0900

    Revert "AjaxAppの準備"
    This reverts commit aa333a16f1c1f06275c392678f09ae7816f94a86.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,12 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all.order(id: "DESC")
+    @posts = Post.all
+  end
+
+  def new
   end
 
   def create
     Post.create(content: params[:content])
-    redirect_to action: :index
   end
 end
